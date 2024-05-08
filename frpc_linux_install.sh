0616c055068b003982803ae235e75e03803e738b
uuxia <263996097@qq.com>
2024-05-08 10:30:41 +0800

    Update frpc_linux_install.sh

--- frpc_linux_install.sh
+++ frpc_linux_install.sh
@@ -18,8 +18,93 @@ FRP_VERSION=0.57.0
 FRP_PATH=/usr/local/frp
 PROXY_URL="https://mirror.ghproxy.com/"
 
-# check frpc
-if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ];then
+function killfrpc() {
+  while ! test -z "$(ps -A | grep -w ${FRP_NAME})"; do
+    FRPCPID=$(ps -A | grep -w ${FRP_NAME} | awk 'NR==1 {print $1}')
+    kill -9 $FRPCPID
+  done
+}
+
+function checkenv() {
+  # check pkg
+  if type apt-get >/dev/null 2>&1; then
+    if ! type wget >/dev/null 2>&1; then
+      apt-get install wget -y
+    fi
+    if ! type curl >/dev/null 2>&1; then
+      apt-get install curl -y
+    fi
+  fi
+
+  if type yum >/dev/null 2>&1; then
+    if ! type wget >/dev/null 2>&1; then
+      yum install wget -y
+    fi
+    if ! type curl >/dev/null 2>&1; then
+      yum install curl -y
+    fi
+  fi
+
+}
+
+function download() {
+  # check network
+  GOOGLE_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "https://www.google.com")
+  PROXY_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "${PROXY_URL}")
+
+  # check arch
+  if [ $(uname -m) = "x86_64" ]; then
+    PLATFORM=amd64
+  elif [ $(uname -m) = "aarch64" ]; then
+    PLATFORM=arm64
+  elif [ $(uname -m) = "armv7" ]; then
+    PLATFORM=arm
+  elif [ $(uname -m) = "armv7l" ]; then
+    PLATFORM=arm
+  elif [ $(uname -m) = "armhf" ]; then
+    PLATFORM=arm
+  fi
+
+  FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
+
+  # download
+  if [ $GOOGLE_HTTP_CODE == "200" ]; then
+    wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+  else
+    if [ $PROXY_HTTP_CODE == "200" ]; then
+      wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+    else
+      echo -e "${Red}检测 GitHub Proxy 代理失效 开始使用官方地址下载${Font}"
+      wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
+    fi
+  fi
+  tar -zxvf ${FILE_NAME}.tar.gz
+
+  mkdir -p ${FRP_PATH}
+  mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
+
+  configure
+
+  # finish install
+  systemctl daemon-reload
+  sudo systemctl start ${FRP_NAME}
+  sudo systemctl enable ${FRP_NAME}
+
+  # clean
+  rm -rf ${WORK_PATH}/${FILE_NAME}.tar.gz ${WORK_PATH}/${FILE_NAME} ${FRP_NAME}_linux_install.sh
+
+  echo -e "${Green}====================================================================${Font}"
+  echo -e "${Green}安装成功,请先修改 ${FRP_NAME}.toml 文件,确保格式及配置正确无误!${Font}"
+  echo -e "${Red}vi /usr/local/frp/${FRP_NAME}.toml${Font}"
+  echo -e "${Green}修改完毕后执行以下命令重启服务:${Font}"
+  echo -e "${Red}sudo systemctl restart ${FRP_NAME}${Font}"
+  echo -e "${Green}====================================================================${Font}"
+
+}
+
+function check() {
+  # check frpc
+  if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml" ] || [ -f "/lib/systemd/system/${FRP_NAME}.service" ]; then
     echo -e "${Green}=========================================================================${Font}"
     echo -e "${RedBG}当前已退出脚本.${Font}"
     echo -e "${Green}检查到服务器已安装${Font} ${Red}${FRP_NAME}${Font}"
@@ -30,99 +115,47 @@ if [ -f "/usr/local/frp/${FRP_NAME}" ] || [ -f "/usr/local/frp/${FRP_NAME}.toml"
     echo -e "${Red}rm -rf /lib/systemd/system/${FRP_NAME}.service${Font}"
     echo -e "${Green}=========================================================================${Font}"
     exit 0
-fi
+  fi
 
-while ! test -z "$(ps -A | grep -w ${FRP_NAME})"; do
-    FRPCPID=$(ps -A | grep -w ${FRP_NAME} | awk 'NR==1 {print $1}')
-    kill -9 $FRPCPID
-done
+  # kill frpc
+  killfrpc
+  # check env
+  checkenv
+}
 
-# check pkg
-if type apt-get >/dev/null 2>&1 ; then
-    if ! type wget >/dev/null 2>&1 ; then
-        apt-get install wget -y
-    fi
-    if ! type curl >/dev/null 2>&1 ; then
-        apt-get install curl -y
-    fi
-fi
+function configure() {
+  # configure frpc.toml
+  echo -e "${Green}请输入Frps服务器地址:${Font}"
+  read server_host
+  echo -e "${Green}请输入Frps服务器端口:${Font}"
+  read server_port
+  echo -e "${Green}请输入Frps的Token:${Font}"
+  read server_token
+  echo -e "${Green}请输入tcp服务名称:${Font}"
+  read tcp_type_name
+  echo -e "${Green}请输入tcp本地端口:${Font}"
+  read tcp_local_port
+  echo -e "${Green}请输入tcp代理端口:${Font}"
+  read tcp_remote_port
 
-if type yum >/dev/null 2>&1 ; then
-    if ! type wget >/dev/null 2>&1 ; then
-        yum install wget -y
-    fi
-    if ! type curl >/dev/null 2>&1 ; then
-        yum install curl -y
-    fi
-fi
-
-# check network
-GOOGLE_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "https://www.google.com")
-PROXY_HTTP_CODE=$(curl -o /dev/null --connect-timeout 5 --max-time 8 -s --head -w "%{http_code}" "${PROXY_URL}")
-
-# check arch
-if [ $(uname -m) = "x86_64" ]; then
-    PLATFORM=amd64
-elif [ $(uname -m) = "aarch64" ]; then
-    PLATFORM=arm64
-elif [ $(uname -m) = "armv7" ]; then
-    PLATFORM=arm
-elif [ $(uname -m) = "armv7l" ]; then
-    PLATFORM=arm
-elif [ $(uname -m) = "armhf" ]; then
-    PLATFORM=arm
-fi
-
-FILE_NAME=frp_${FRP_VERSION}_linux_${PLATFORM}
-
-# download
-if [ $GOOGLE_HTTP_CODE == "200" ]; then
-    wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
-else
-    if [ $PROXY_HTTP_CODE == "200" ]; then
-        wget -P ${WORK_PATH} ${PROXY_URL}https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
-    else
-        echo -e "${Red}检测 GitHub Proxy 代理失效 开始使用官方地址下载${Font}"
-        wget -P ${WORK_PATH} https://github.com/fatedier/frp/releases/download/v${FRP_VERSION}/${FILE_NAME}.tar.gz -O ${FILE_NAME}.tar.gz
-    fi
-fi
-tar -zxvf ${FILE_NAME}.tar.gz
-
-mkdir -p ${FRP_PATH}
-mv ${FILE_NAME}/${FRP_NAME} ${FRP_PATH}
-
-# configure frpc.toml
-RADOM_NAME=$(cat /dev/urandom | head -n 10 | md5sum | head -c 8)
-cat >${FRP_PATH}/${FRP_NAME}.toml<<EOF
-serverAddr = "uuxia.cn"
-serverPort = 6000
-auth.token = "xiaxiaoli1"
+  RADOM_NAME=$(cat /dev/urandom | head -n 10 | md5sum | head -c 8)
+  cat >${FRP_PATH}/${FRP_NAME}.toml <<EOF
+serverAddr = "$server_host"
+serverPort = $server_port
+auth.token = "$server_token"
 
 [[proxies]]
-name = "lonbon_${RADOM_NAME}"
-type = "https"
-localIP = "192.168.1.2"
-localPort = 5001
-customDomains = ["nas.yourdomain.com"]
-
-[[proxies]]
-name = "chengguan-lonbon_${RADOM_NAME}"
+name = "${tcp_type_name}_${RADOM_NAME}"
 type = "tcp"
 localIP = "0.0.0.0"
-localPort = 22
-remotePort = 6304
-
-[[proxies]]
-name = "chengguan-lonbon_web_${RADOM_NAME}"
-type = "tcp"
-localIP = "0.0.0.0"
-localPort = 8080
-remotePort = 6305
-
+localPort = $tcp_local_port
+remotePort = $tcp_remote_port
 EOF
 
-# configure systemd
-cat >/lib/systemd/system/${FRP_NAME}.service <<EOF
+
+
+  # configure systemd
+  cat >/lib/systemd/system/${FRP_NAME}.service <<EOF
 [Unit]
 Description=Frp Server Service
 After=network.target syslog.target
@@ -138,17 +171,48 @@ ExecStart=/usr/local/frp/${FRP_NAME} -c /usr/local/frp/${FRP_NAME}.toml
 WantedBy=multi-user.target
 EOF
 
-# finish install
-systemctl daemon-reload
-sudo systemctl start ${FRP_NAME}
-sudo systemctl enable ${FRP_NAME}
+}
 
-# clean
-rm -rf ${WORK_PATH}/${FILE_NAME}.tar.gz ${WORK_PATH}/${FILE_NAME} ${FRP_NAME}_linux_install.sh
+function test() {
+  echo -e "${Green}请输入Frps服务器地址:${Font}"
+  read server_host
+  echo -e "${Green}请输入Frps服务器端口:${Font}"
+  read server_port
+  echo -e "${Green}请输入Frps的Token:${Font}"
+  read server_token
+  echo -e "${Green}请输入tcp服务名称:${Font}"
+  read tcp_type_name
+  echo -e "${Green}请输入tcp本地端口:${Font}"
+  read tcp_local_port
+  echo -e "${Green}请输入tcp代理端口:${Font}"
+  read tcp_remote_port
+  RADOM_NAME=m2
+  cat >/Users/uuxia/Downloads/frpc/frpc_test.toml <<EOF
+serverAddr = "$server_host"
+serverPort = $server_port
+auth.token = "$server_token"
 
-echo -e "${Green}====================================================================${Font}"
-echo -e "${Green}安装成功,请先修改 ${FRP_NAME}.toml 文件,确保格式及配置正确无误!${Font}"
-echo -e "${Red}vi /usr/local/frp/${FRP_NAME}.toml${Font}"
-echo -e "${Green}修改完毕后执行以下命令重启服务:${Font}"
-echo -e "${Red}sudo systemctl restart ${FRP_NAME}${Font}"
-echo -e "${Green}====================================================================${Font}"
+#[[proxies]]
+#name = "lonbon_${RADOM_NAME}"
+#type = "https"
+#localIP = "192.168.1.2"
+#localPort = 5001
+#customDomains = ["nas.yourdomain.com"]
+
+[[proxies]]
+name = "${tcp_type_name}_${RADOM_NAME}"
+type = "tcp"
+localIP = "0.0.0.0"
+localPort = $tcp_local_port
+remotePort = $tcp_remote_port
+
+EOF
+
+}
+
+function main() {
+  check
+  download
+}
+
+main
